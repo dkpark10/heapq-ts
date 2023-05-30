ba478010017fd7f940f12c740beae754b997f1c1
dkpark10 <dkpark10@naver.com>
2023-05-30 18:28:18 +0900

    chore: 테스트 경로 추가

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '@/(.*)$': '<rootDir>/src/$1',
   },
   testEnvironment: 'node',
+  testMatch: ["**/__test__/**/*.[jt]s?(x)"],
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
